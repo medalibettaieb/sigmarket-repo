25881746c5220b0b6edbd23cd066b3be1d33df6e
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-10-18 10:52:47 +0200

    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- sigmarket-client/src/main/java/purchaseGui/TestPurchaseProcess.java
+++ sigmarket-client/src/main/java/purchaseGui/TestPurchaseProcess.java
@@ -11,7 +11,7 @@ public class TestPurchaseProcess {
 	public static void main(String[] args) throws NamingException {
 		Context context = new InitialContext();
 		PurchaseManagementRemote purchaseManagementRemote = (PurchaseManagementRemote) context.lookup(
-				"sigmarket-ear/sigmarket-ejb/PurchaseManagement!tn.esprit.sigma.sigmarket.services.PurchaseManagementRemote");
+				"sigmarket-ear/sigmarket-ejb/PurchaseManagement!tn.esprit.sigma.sigmarket.services.interfaces.PurchaseManagementRemote");
 
 		purchaseManagementRemote.purchase(3, 1, 10);
 	}
